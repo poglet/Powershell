fa13e776f6c9856a7b7fbd121dffaa467c1a2726
poglet <christhomas@fastmail.com>
2016-08-20 17:35:51 +1000

    sdfsdf
    sdf

--- stop process.ps1
+++ stop process.ps1
@@ -1,3 +1,4 @@
-﻿$ProcessID = get-process | Select-Object Id, pm, processname | Sort-Object PM -Descending | select-object -first 1
+﻿$ProcessID = get-process -ComputerName POGLET | Select-Object Id, pm, processname | Sort-Object PM -Descending | select-object -first 1
 stop-process -id $ProcessID.id -Force -Confirm
 
+testtig brabcg
